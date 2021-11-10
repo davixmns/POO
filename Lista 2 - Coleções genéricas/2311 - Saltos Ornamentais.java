98646e989aca50997319b544ee10c791b31a32a4
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2021-11-10 19:33:44 -0300

    Update 2311 - Saltos Ornamentais.java

--- Lista 2 - Coleções genéricas/2311 - Saltos Ornamentais.java
+++ Lista 2 - Coleções genéricas/2311 - Saltos Ornamentais.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.IOException;
 
-//Problema: https://www.urionlinejudge.com.br/judge/pt/problems/view/2311
+//Problema: https://www.beecrowd.com.br/judge/pt/problems/view/2311
 
 public class Main {
     static class Saltadora {
@@ -26,30 +26,20 @@ public class Main {
         }
 
     }
-
-
+    
     static class Juizes {
-        ArrayList<Double> lista = new ArrayList<Double>();
-
-        public void darNota(double nota) {
-            lista.add(nota);
-        }
-
-        public void limparLista() {
-            lista.clear();
-        }
+        ArrayList<Double> listaDeSaltos = new ArrayList<Double>();
 
         public double calcularNota(Saltadora saltadora) {
-            lista.sort(null);
+            listaDeSaltos.sort(null);
             double soma = 0;
-            for (int i = 0; i < lista.size(); i++) {
-                soma += lista.get(i);
+            for (int i = 0; i < listaDeSaltos.size(); i++) {
+                soma += listaDeSaltos.get(i);
             }
-            return (soma - (lista.get(0) + lista.get(6))) * saltadora.getDificuldade();
+            return (soma - (listaDeSaltos.get(0) + listaDeSaltos.get(6))) * saltadora.getDificuldade();
         }
     }
-
-
+    
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         Juizes juizes = new Juizes();
@@ -61,11 +51,11 @@ public class Main {
             saltadora.setDificuldade(scanner.nextDouble());
 
             for (int j = 0; j < 7; j++) {
-                juizes.darNota(scanner.nextDouble());
+                juizes.listaDeSaltos.add(scanner.nextDouble());
             }
             System.out.print(saltadora.getNomeSaltadora() + " ");
             System.out.printf("%.2f\n", juizes.calcularNota(saltadora));
-            juizes.limparLista();
+            juizes.listaDeSaltos.clear();
         }
     }
 }
