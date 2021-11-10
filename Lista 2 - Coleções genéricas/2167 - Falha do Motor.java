18acd8fc99609a8af7874cc56367602583573e44
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2021-11-10 18:24:55 -0300

    Update 2167 - Falha do Motor.java

--- Lista 2 - Coleções genéricas/2167 - Falha do Motor.java
+++ Lista 2 - Coleções genéricas/2167 - Falha do Motor.java
@@ -2,15 +2,17 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.IOException;
 
+//Problema: https://www.beecrowd.com.br/judge/pt/problems/view/2167
+
 public class Main {
     static class testeDoMotor {
-        ArrayList<Integer> lista = new ArrayList<Integer>();
+        ArrayList<Integer> lista = new ArrayList<>();
 
         public void ligarMotor(int velocidade) {
             lista.add(velocidade);
         }
 
-        public int detectorDeFalhas() {
+        public int detectorDeQuedas() {
             int falha = 0;
             for (int i = 1; i < lista.size(); i++) {
                 if (lista.get(i) < lista.get(i - 1)) {
@@ -23,7 +25,6 @@ public class Main {
 
     }
 
-
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         testeDoMotor motor = new testeDoMotor();
@@ -32,6 +33,6 @@ public class Main {
         for (int i = 0; i < x; i++) {
             motor.ligarMotor(scanner.nextInt());
         }
-        System.out.println(motor.detectorDeFalhas());
+        System.out.println(motor.detectorDeQuedas());
     }
 }
