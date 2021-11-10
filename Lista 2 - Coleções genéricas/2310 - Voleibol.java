6ba716719cf47bc386864e9475cbe7620ec22b34
Davi Ximenes <82062555+davixmns@users.noreply.github.com>
2021-11-10 18:30:43 -0300

    Update 2310 - Voleibol.java

--- Lista 2 - Coleções genéricas/2310 - Voleibol.java
+++ Lista 2 - Coleções genéricas/2310 - Voleibol.java
@@ -12,57 +12,45 @@ public class Main {
             jogadores.add(nome);
         }
     }
-    static class Jogo {
+    static class Partida {
         ArrayList<Double> listaDeSaques = new ArrayList<>();
         ArrayList<Double> listaDeBloqueios = new ArrayList<>();
         ArrayList<Double> listaDeAtaques = new ArrayList<>();
-
-        public int tamListaDeSaques(){ return listaDeAtaques.size(); }
-        public int tamListaDeBloqueios(){ return listaDeBloqueios.size(); }
-        public int tamListaDeAtaques(){ return listaDeBloqueios.size(); }
-
-        public double getSaque(int i) { return listaDeSaques.get(i); }
-        public double getBloqueio(int i) { return listaDeBloqueios.get(i); }
-        public double getAtaque(int i) { return listaDeAtaques.get(i); }
-
-        public void realizarSaque(double saque) { listaDeSaques.add(saque); }
-        public void realizarBloqueio(double bloqueio) { listaDeBloqueios.add(bloqueio); }
-        public void realizarAtaque(double ataque) { listaDeAtaques.add(ataque); }
     }
 
     static class Juizes {
-        public double avaliarSaques(Jogo jogo) { //Calcula pontos dos saques
+        public double avaliarSaques(Partida jogo) { //Calcula pontos dos saques
             double saquesTotais = 0;
             double saquesSucedidos = 0;
-            for (int i = 0; i < jogo.tamListaDeSaques(); i += 2) {
-                saquesTotais += jogo.getSaque(i);
+            for (int i = 0; i < jogo.listaDeSaques.size(); i += 2) {
+                saquesTotais += jogo.listaDeSaques.get(i);
             }
-            for (int i = 1; i < jogo.tamListaDeSaques(); i += 2) {
-                saquesSucedidos += jogo.getSaque(i);
+            for (int i = 1; i < jogo.listaDeSaques.size(); i += 2) {
+                saquesSucedidos += jogo.listaDeSaques.get(i);
             }
             return saquesSucedidos * 100 / saquesTotais;
         }
 
-        public double avaliarBloqueios(Jogo jogo) { //Calcula pontos dos bloqueios
+        public double avaliarBloqueios(Partida jogo) { //Calcula pontos dos bloqueios
             double bloqueiosTotais = 0;
             double bloqueiosSucedidos = 0;
-            for (int i = 0; i < jogo.tamListaDeBloqueios(); i += 2) {
-                bloqueiosTotais += jogo.getBloqueio(i);
+            for (int i = 0; i < jogo.listaDeBloqueios.size(); i += 2) {
+                bloqueiosTotais += jogo.listaDeBloqueios.get(i);
             }
-            for (int i = 1; i < jogo.tamListaDeBloqueios(); i += 2) {
-                bloqueiosSucedidos += jogo.getBloqueio(i);
+            for (int i = 1; i < jogo.listaDeBloqueios.size(); i += 2) {
+                bloqueiosSucedidos += jogo.listaDeBloqueios.get(i);
             }
             return bloqueiosSucedidos * 100 / bloqueiosTotais;
         }
 
-        public double avaliarAtaques(Jogo jogo) { //Calcula pontos dos Ataques
+        public double avaliarAtaques(Partida jogo) { //Calcula pontos dos Ataques
             double ataquesTotais = 0;
             double ataquesSucedidos = 0;
-            for (int i = 0; i < jogo.tamListaDeAtaques(); i += 2) {
-                ataquesTotais += jogo.getAtaque(i);
+            for (int i = 0; i < jogo.listaDeAtaques.size(); i += 2) {
+                ataquesTotais += jogo.listaDeAtaques.get(i);
             }
-            for (int i = 1; i < jogo.tamListaDeAtaques(); i += 2) {
-                ataquesSucedidos += jogo.getAtaque(i);
+            for (int i = 1; i < jogo.listaDeAtaques.size(); i += 2) {
+                ataquesSucedidos += jogo.listaDeAtaques.get(i);
             }
             return ataquesSucedidos * 100 / ataquesTotais;
         }
@@ -72,20 +60,20 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Jogadores jogadores = new Jogadores();
         Juizes juizes = new Juizes();
-        Jogo jogo = new Jogo();
+        Partida partida = new Partida();
 
         int nJogadores = scanner.nextInt();
         for (int i = 0; i < nJogadores; i++) {
             jogadores.addNomeDoJogador(scanner.next());
             for (int j = 0; j < 2; j++) {
-                jogo.realizarSaque(scanner.nextDouble());
-                jogo.realizarBloqueio(scanner.nextDouble());
-                jogo.realizarAtaque(scanner.nextDouble());
+                partida.listaDeSaques.add(scanner.nextDouble());
+                partida.listaDeBloqueios.add(scanner.nextDouble());
+                partida.listaDeAtaques.add(scanner.nextDouble());
             }
         }
 
-        System.out.printf("Pontos de Saque: %.2f %%.\n", juizes.avaliarSaques(jogo));
-        System.out.printf("Pontos de Bloqueio: %.2f %%.\n", juizes.avaliarBloqueios(jogo));
-        System.out.printf("Pontos de Ataque: %.2f %%.\n", juizes.avaliarAtaques(jogo));
+        System.out.printf("Pontos de Saque: %.2f %%.\n", juizes.avaliarSaques(partida));
+        System.out.printf("Pontos de Bloqueio: %.2f %%.\n", juizes.avaliarBloqueios(partida));
+        System.out.printf("Pontos de Ataque: %.2f %%.\n", juizes.avaliarAtaques(partida));
     }
 }
